d27cc08d0c1613ca3d5f09a419ebdf7cac44db16
kmgyun0707 <128140112+kmgyun0707@users.noreply.github.com>
2024-01-25 14:44:21 +0900

    Add files via upload

---
+++ pepper_guid_main.py
@@ -0,0 +1,30 @@
+#로봇 페퍼가 가이드 역할을 하는 프로그램입니다.
+
+class PepperGuideMain:
+
+    #생성자
+    def __init__(self):
+        pass
+
+    #페퍼가 사람을 인식하면,
+    def on_people_detected(self, value):
+        pass
+ 
+    #SLAM을 실행합니다.
+    def start_slam(self):
+        pass
+
+    #SLAM을 통해 길을 안내합니다.
+    def guide(self):
+        pass
+
+    #페퍼가 사람에게 인사를 합니다.
+    def say_hello(self):
+        pass
+
+
+ #메인 함수
+if __name__ == "__main__":
+    pgm = PepperGuideMain()
+    pass
+
